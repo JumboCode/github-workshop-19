14c5a9732e4c88530a3de3569693d242e50f33d9
Manish Aryal <manisharyal@Manishs-MacBook-Pro.local>
2019-09-29 15:40:41 -0400

    typo fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get('/', (req, res) => "Hello world!")
+app.get('/', (req, res) => res.send("Hello world!"))
  
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
